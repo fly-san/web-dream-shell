51f45e24ca069ed0423d61ad528dcf49bd22fddb
Dominik Klein <dominik.klein@tum.de>
2022-11-19 17:34:57 +0100

    Added NavBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import { Nav, Row, Col } from "react-bootstrap";
+import { ReactComponent as Snail } from "./assets/snail.svg";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { BankOffers } from "./components/BankOffers";
+import { LinkContainer } from "react-router-bootstrap";
 
 function App() {
-  return <BankOffers />;
+  return (
+    <Router>
+      <Col>
+        <Row xs="auto" className="align-items-center" id="main-navigation-bar">
+          <Col>
+            <Snail id="navigation-bar-icon" />
+          </Col>
+          <Col>
+            <h2>Dream Shell</h2>
+          </Col>
+          <Col>
+            <Nav variant="pills" defaultActiveKey="/home">
+              <Nav.Item>
+                <LinkContainer to="/home">
+                  <Nav.Link>Home</Nav.Link>
+                </LinkContainer>
+              </Nav.Item>
+              <Nav.Item>
+                <LinkContainer to="/offers">
+                  <Nav.Link>Offers</Nav.Link>
+                </LinkContainer>
+              </Nav.Item>
+            </Nav>
+          </Col>
+        </Row>
+        <Row>
+          <Routes>
+            <Route path="/offers" element={<BankOffers />} />
+            <Route path="/home" element={<h2>Home</h2>} />
+            <Route path="/" element={<Navigate to="/home" />} />
+          </Routes>
+        </Row>
+      </Col>
+    </Router>
+  );
 }
 
 export default App;
