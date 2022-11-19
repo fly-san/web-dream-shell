840d308766f6b157aad1f845f0a05ed85be0133d
Dominik Klein <dominik.klein@tum.de>
2022-11-19 19:30:19 +0100

    Fixed vertical navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Nav, Row, Col } from "react-bootstrap";
+import { Nav, Row, Col, Container } from "react-bootstrap";
 import { ReactComponent as Snail } from "./assets/snail.svg";
 import {
   BrowserRouter as Router,
@@ -14,16 +14,18 @@ import { HomePage } from "./components/HomePage";
 function App() {
   return (
     <Router>
-      <Col>
-        <Row xs="auto" className="align-items-center" id="main-navigation-bar">
-          <Col>
-            <Snail id="navigation-bar-icon" />
-          </Col>
-          <Col>
-            <h2>Dream Shell</h2>
-          </Col>
-          <Col>
-            <Nav variant="pills" defaultActiveKey="/home">
+      <Container fluid>
+        <Row>
+          <Col xs={2} id="main-navigation-bar">
+            <Row>
+              <Snail id="navigation-bar-icon" />
+              <h2>Dream Shell</h2>
+            </Row>
+            <Nav
+              className="flex-column"
+              variant="pills"
+              defaultActiveKey="/home"
+            >
               <Nav.Item>
                 <LinkContainer to="/home">
                   <Nav.Link>Home</Nav.Link>
@@ -36,15 +38,17 @@ function App() {
               </Nav.Item>
             </Nav>
           </Col>
+          <Col style={{ padding: 0 }}>
+            <Container fluid id="main-container">
+              <Routes>
+                <Route path="/offers" element={<BankOffers />} />
+                <Route path="/home" element={<HomePage />} />
+                <Route path="/" element={<Navigate to="/home" />} />
+              </Routes>
+            </Container>
+          </Col>
         </Row>
-        <Row>
-          <Routes>
-            <Route path="/offers" element={<BankOffers />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/" element={<Navigate to="/home" />} />
-          </Routes>
-        </Row>
-      </Col>
+      </Container>
     </Router>
   );
 }
