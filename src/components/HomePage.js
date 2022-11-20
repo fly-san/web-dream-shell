32dd7bf13cfaf23684237da487d470a8248b2f0d
Jingyi Jia <jingyi.jia@tum.de>
2022-11-20 04:45:15 +0100

    change homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,113 +6,147 @@ import Col from "react-bootstrap/Col";
 import { useContext } from "react";
 import { EquityContext } from "../App";
 import { useNavigate } from "react-router-dom";
+import Card from 'react-bootstrap/Card';
+import "./HomePage.css"
+import ParticlesBg from 'particles-bg'
 
 export function HomePage(props) {
   const equityContext = useContext(EquityContext);
   const navigate = useNavigate();
+  
+  
+  
+
   return (
     <div>
-      <Container>
-        <Row>
-          {/* left size about house */}
-          <Col sm={8}>
-            <h4>Tell me more about your dream shell:</h4>
-            <Form>
-              {/* Location */}
-              <Form.Group className="mb-3" controlId="location">
-                <Form.Label>Where?</Form.Label>
-                <Form.Control type="text" placeholder="Zip or City" />
-              </Form.Group>
+      <h1 class="slogen">East or West,</h1>
+      <h3 class="slogen">Home is the best!</h3>
+      <p></p>
 
-              {/* Room Numbers */}
-              <Form.Label>Room Numbers:</Form.Label>
-              <Row>
-                <Col>
-                  {/* Min Number */}
-                  <Form.Group className="mb-3" controlId="roomNumbersMin">
-                    <Form.Control type="text" placeholder="Min" />
+      <Row>
+        <Col>
+          <Card border="dark" style={{ width: '30rem' }}>
+            <Card.Header>Your personal information</Card.Header>
+            <Card.Body>         
+              <Form>
+                {/* ENquity */}
+                <Form.Label>Your Savings:</Form.Label>
+                <Form.Group className="mb-3" controlId="equity">
+                  <Row>
+                    <Col>
+                      <Form.Control
+                        type="text"
+                        placeholder=""
+                        onChange={(value) => equityContext.setEquity(value)}
+                      />
+                    </Col>
+                    <Col>Euro</Col>
+                  </Row>
+                </Form.Group>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Col>
+
+        <Col md="auto">
+          <Card border="dark" style={{ width: '35em' }}>
+            <Card.Header>Tell me more about your dream shell</Card.Header>
+            <Card.Body>
+              {/* left size about house */}
+              <p></p>
+              <Form>          
+                  {/* Location */}
+                  <Form.Group as={Row} className="mb-3" controlId="location">
+                    <Form.Label column sm={3}>Where?</Form.Label>
+                    <Col sm={5}><Form.Control type="text" placeholder="Zip or City" /></Col>
                   </Form.Group>
-                </Col>
-                <Col md="auto">to</Col>
-                <Col>
-                  {/* Max number */}
-                  <Form.Group className="mb-3" controlId="roomNumbersMax">
-                    <Form.Control type="text" placeholder="Max" />
-                  </Form.Group>
-                </Col>
-              </Row>
+                  <p></p>
+                  {/* Room Numbers */}             
+                  <Row>
+                  <p></p>
+                    <Col md="auto"><Form.Label>Room Numbers:</Form.Label></Col>
+                    <Col sm={3}>
+                      {/* Min Number */}
+                      <Form.Group className="mb-3" controlId="roomNumbersMin">
+                        <Form.Control type="text" placeholder="Min" />
+                      </Form.Group>
+                    </Col>
+                    <Col md="auto">to</Col>
+                    <Col sm={3}>
+                      {/* Max number */}
+                      <Form.Group className="mb-3" controlId="roomNumbersMax">
+                        <Form.Control type="text" placeholder="Max" />
+                      </Form.Group>
+                    </Col>
+                  </Row>
+                  <p></p>
+                  {/* Size */}
+                  <Row>
+                    <Col md="auto"><Form.Label>Size:</Form.Label></Col>
+                    <Col sm={5}>
+                      <Form.Group className="mb-3" controlId="roomSize">
+                        <Row>
+                          <Col>
+                            <Form.Control type="text" placeholder="" />
+                          </Col>
+                          <Col>
+                            m<sup>2</sup>
+                          </Col>
+                        </Row>
+                      </Form.Group>
+                    </Col>
+                  </Row>
+                  <p></p>
+                  {/* Price */}
+                  <Row>
+                    <Col md="auto">
+                      <Form.Label>Total Price: About</Form.Label>
+                    </Col>
+                    
+                    <Col sm={3}>
+                      <Form.Group className="mb-3" controlId="prise">
+                            <Form.Control type="text" placeholder="" />
+                      </Form.Group>
+                    </Col>
+                    <Col>Euro</Col>   
+                  </Row>  
 
-              {/* Size */}
-              <Form.Label>Size:</Form.Label>
-              <Form.Group className="mb-3" controlId="roomSize">
-                <Row>
-                  <Col>
-                    <Form.Control type="text" placeholder="" />
-                  </Col>
-                  <Col>
-                    m<sup>2</sup>
-                  </Col>
-                </Row>
-              </Form.Group>
+                  <p></p>
+                  
+                  {/* Housing Type */}
+                  <Row>
+                    <Col md="auto"><Form.Label>Housing Type:</Form.Label></Col>
+                    <Col>
+                      <Form.Group className="mb-3" controlId="houseCheckbox">
+                        <Form.Check type="checkbox" label="House" />
+                      </Form.Group>
+                      <Form.Group className="mb-3" controlId="apartmentCheckbox">
+                        <Form.Check type="checkbox" label="Apartment" />
+                      </Form.Group>
+                    </Col>
+                  </Row>            
+              </Form>
+            </Card.Body>
+          </Card>
+        </Col>
 
-              {/* Price */}
-              <Form.Label>Total Price:</Form.Label>
-              <Form.Group className="mb-3" controlId="prise">
-                <Row>
-                  <Col>About</Col>
-                  <Col>
-                    <Form.Control type="text" placeholder="" />
-                  </Col>
-                  <Col>Euro</Col>
-                </Row>
-              </Form.Group>
-
-              {/* Housing Type */}
-              <Form.Label>Housing Type:</Form.Label>
-              <Form.Group className="mb-3" controlId="houseCheckbox">
-                <Form.Check type="checkbox" label="House" />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="apartmentCheckbox">
-                <Form.Check type="checkbox" label="Apartment" />
-              </Form.Group>
-            </Form>
-          </Col>
-
-          <Col sm={4}>
-            <h4>Your personal information: </h4>
-            <Form>
-              {/* ENquity */}
-              <Form.Label>Your Saving:</Form.Label>
-              <Form.Group className="mb-3" controlId="equity">
-                <Row>
-                  <Col>
-                    <Form.Control
-                      type="text"
-                      placeholder=""
-                      onChange={(value) => equityContext.setEquity(value)}
-                    />
-                  </Col>
-                  <Col>Euro</Col>
-                </Row>
-              </Form.Group>
-            </Form>
-          </Col>
-        </Row>
-
-        <Row>
-          <Col md={{ span: 4, offset: 10 }}>
-            <Button
-              variant="dark"
-              type="submit"
-              onClick={() => {
-                navigate("/offers");
-              }}
-            >
-              Next Step
-            </Button>
-          </Col>
-        </Row>
-      </Container>
+        
+      </Row>
+   
+      <p></p>
+      <p></p>
+      <Button
+        variant="light"
+        type="submit"
+        onClick={() => {
+          navigate("/offers");
+        }}
+        style={{float:"right", margin:"2em"}}
+      >
+        Next Step
+      </Button>
+      <ParticlesBg color="#f0ffff" type="cobweb" bg={true} />    
+      
     </div>
   );
 }
